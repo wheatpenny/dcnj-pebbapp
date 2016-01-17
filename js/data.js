c9dc4332a6642137464c9efea277b0ac68007677
Levi Sigworth <levis@alumni.cmu.edu>
2016-01-17 12:46:49 -0500

    Add JSONP back.

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@
 //When DOM loaded we attach click event to button
 $(document).ready(function() {
 
-  var sessionsAPI = "https://www.drupalcampnj.org/program/sessions/json";
+  var sessionsAPI = "https://www.drupalcampnj.org/program/sessions/json?callback=?";
   var jqxhr = $.getJSON( sessionsAPI, function() {
     console.log( "success" );
   })
