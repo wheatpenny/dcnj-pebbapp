a384df4b0e3fd726b45a3c8fe34e7e0cbb5e58e1
Levi Sigworth <levis@alumni.cmu.edu>
2016-01-17 12:38:25 -0500

    Test with new syntax.

--- js/data.js
+++ js/data.js
@@ -2,21 +2,11 @@
 //When DOM loaded we attach click event to button
 $(document).ready(function() {
 
-  //start ajax request
-  $.ajax({
-      url: "https://www.drupalcampnj.org/program/sessions/json",
-      //force to handle it as text
-      dataType: "text",
-      success: function(data) {
-
-          //data downloaded so we call parseJSON function
-          //and pass downloaded data
-          var json = $.parseJSON(data);
-          console.log(json);
-          //now json variable contains data in json format
-          //let's display a few items
-          //$('#results').html('Plugin name: ' + json.name + '<br />Author: ' + json.author.name);
-      }
+  var sessionsAPI = "https://www.drupalcampnj.org/program/sessions/json?callback=?"
+  var jqxhr = $.getJSON( sessionsAPI, function() {
+    console.log( "success" );
+  })
+  .fail(function() {
+    console.log( "error" );
   });
-
 });
